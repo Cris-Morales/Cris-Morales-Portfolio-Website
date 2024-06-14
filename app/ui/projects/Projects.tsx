8efca126a4d492dee9e7fb08e7c8bae291a2cdb4
Cristian Morales <CrisMorales@protonmail.com>
2024-06-14 12:48:13 -0500

    html scaffolding checkpoint. navbar, header, file/page layout

--- app/ui/projects/Projects.tsx
+++ app/ui/projects/Projects.tsx
@@ -1,7 +1,7 @@
 'use client'
 import Image from "next/image"
 
-export default function Hero() {
+export default function Projects() {
 
     return (
         <section id='projects'>
--- app/ui/projects/talksAndPubs/TalksAndPubs.tsx
+++ app/ui/projects/talksAndPubs/TalksAndPubs.tsx
@@ -1,13 +1,20 @@
 'use client'
 import Image from "next/image"
 
-export default function Hero() {
+export default function TalksAndPubs() {
 
     return (
         <section id='talks'>
             <Image
-                src="/headshot-nobg.png"
-                alt="Headshot of me, at my desk, trying to look thoughtful and professional."
+                src="/projects/talksAndPubs/Docketeer15-Article-Icon.png"
+                alt="External Link Thumbnail to Medium Article on the Release of Docketeer 15"
+                width={454}
+                height={530.11}
+                priority
+            />
+            <Image
+                src="/projects/talksAndPubs/TechTalk.png"
+                alt="External Link Thumbnail to Tech Talk on The Internet of Things"
                 width={454}
                 height={530.11}
                 priority
