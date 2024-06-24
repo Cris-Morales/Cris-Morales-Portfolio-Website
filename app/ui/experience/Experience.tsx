ccd46ccd0d634682617b2ef85b5353e5eab1f5c3
Cristian Morales <CrisMorales@protonmail.com>
2024-06-23 19:54:26 -0500

    formated bullets to actually be bullets. added a not on expanded bullets in case I would like to add that later

--- app/ui/experience/Experience.tsx
+++ app/ui/experience/Experience.tsx
@@ -4,13 +4,13 @@ import { experienceData } from "@/app/lib/types"
 export default function Experience() {
 
     return (
-        <section id='experience' className="h-fit flex flex-col gap-20">
+        <section id='experience' className="h-fit flex flex-col border border-primary">
             <h3 className='text-primary text-3xl ml-10'>
                 Experience
             </h3>
             {experience.map((experienceData: experienceData) => {
                 return (
-                    <article>
+                    <article className="text-white-text border-2 border-primary rounded-3xl pl-7 pr-5 py-5 m-5 bg-background bg-opacity-35 boxGlow">
                         <h4>
                             {experienceData.title}
                         </h4>
@@ -18,7 +18,7 @@ export default function Experience() {
                             <h5>{experienceData.company}</h5>
                             <h5>{experienceData.timerange}</h5>
                         </div>
-                        <ul>
+                        <ul className=" list-disc">
                             {experienceData.bullets.map((bullet: string) => {
                                 return (
                                     <li>
@@ -29,22 +29,19 @@ export default function Experience() {
                                 )
                             })}
                         </ul>
+                        {/* i wonder if adding this expanded section is too much for software positions. But part of me likes the idea of using this as a catch all website for me */}
                         {experienceData.expandedBullets &&
-                            <div>
-                                EXPANDED BULLETS
-                                <ul>
-                                    {experienceData.expandedBullets.map((bullet: string) => {
-                                        return (
-                                            <li>
-                                                <p>
-                                                    {bullet}
-                                                </p>
-                                            </li>
-                                        )
-                                    })}
-                                </ul>
-                            </div>
-                        }
+                            <ul id='experienceData' className=" list-disc hidden">
+                                {experienceData.expandedBullets.map((bullet: string) => {
+                                    return (
+                                        <li>
+                                            <p>
+                                                {bullet}
+                                            </p>
+                                        </li>
+                                    )
+                                })}
+                            </ul>}
                     </article>
                 )
             })}
