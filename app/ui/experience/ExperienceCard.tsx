aee9313ba1a8304c2b0283af848932d335653816
Cristian Morales <CrisMorales@protonmail.com>
2024-06-30 23:07:29 -0500

    added carret svg that flips with framer instead of expand. it also glows when activated.

--- app/ui/experience/ExperienceCard.tsx
+++ app/ui/experience/ExperienceCard.tsx
@@ -2,6 +2,7 @@
 import { experienceData } from "@/app/lib/types"
 import { useState } from "react"
 import { AnimatePresence, motion, Variants } from 'framer-motion'
+import Image from "next/image"
 
 const itemVariants: Variants = {
     open: {
@@ -9,14 +10,14 @@ const itemVariants: Variants = {
         y: 0,
         transition: { type: "spring", stiffness: 300, damping: 24 }
     },
-    closed: { opacity: 0, y: 20 }
+    closed: { opacity: 0, y: -20 }
 };
 
 export default function ExperienceCard({ data, index }: { data: experienceData, index: number }) {
     const [isExpanded, setExpanded] = useState<boolean>(false);
 
     return (
-        <article className="text-white-text border-2 border-primary rounded-3xl px-7 py-5 m-5 bg-background bg-opacity-35 boxGlow w-11/12 xl:w-3/5">
+        <article className="flex flex-col text-white-text border-2 border-primary rounded-3xl px-7 py-5 m-5 bg-background bg-opacity-35 boxGlow w-11/12 xl:w-7/12">
             <h4 className=" text-xl font-bold">
                 {data.title}
             </h4>
@@ -26,7 +27,7 @@ export default function ExperienceCard({ data, index }: { data: experienceData, 
                 </h4>
                 <h4 className="">{data.timerange}</h4>
             </div>
-            <ul className="list-disc text-base text-light-text opacity-80">
+            <ul className="list-disc text-base text-light-text opacity-80 mx-4">
                 {data.bullets.map((bullet: string) => {
                     return (
                         <li key={bullet}>
@@ -44,13 +45,13 @@ export default function ExperienceCard({ data, index }: { data: experienceData, 
                             initial='closed'
                             animate='open'
                             exit='closed'
-                            className="list-disc text-base text-light-text opacity-80"
+                            className="list-disc text-base text-light-text opacity-80 mx-4"
                             variants={{
                                 open: { height: "auto" },
-                                closed: { height: 0, transition: { delay: 0.15 } },
+                                closed: { height: 0, transition: { delay: 0.1 } },
                             }}
                             transition={{
-                                delayChildren: 0.15,
+                                delayChildren: 0.1,
                                 staggerChildren: 0.05
                             }}
                         >
@@ -68,7 +69,35 @@ export default function ExperienceCard({ data, index }: { data: experienceData, 
                     }
                 </AnimatePresence>
             }
-            {data.expandedBullets && <button className=" font-bold" onClick={() => setExpanded(!isExpanded)}>Expand</button>}
+            {data.expandedBullets &&
+                <motion.button
+                    className="self-end -mb-4 -mt-4 -mr-4 rounded-full"
+                    animate={isExpanded ? "open" : "closed"}
+                    whileTap={{ scale: 0.85 }} onClick={() => setExpanded(!isExpanded)}>
+                    <motion.div
+                        variants={{
+                            open: {
+                                rotate: -180, filter: [
+                                    `drop-shadow(0 0 0.5rem #52FFC2)`,
+                                    `drop-shadow(0 0 1rem #52FFC2)
+                                    drop-shadow(0 0 0.5rem #52FFC2)`,
+                                    `drop-shadow(0 0 1.5rem #52FFC2)
+                                    drop-shadow(0 0 1rem #52FFC2)
+                                    drop-shadow(0 0 0.5rem #52FFC2)`]
+                            },
+                            closed: { rotate: 0, filter: '' }
+                        }}
+                        transition={{ duration: 0.2 }}
+                        style={{ originY: 0.55 }}
+                    >
+                        <Image
+                            src={'/caret-down.svg'}
+                            alt='Expand'
+                            width={32}
+                            height={32}
+                        />
+                    </motion.div>
+                </motion.button>}
         </article>
     )
 }
