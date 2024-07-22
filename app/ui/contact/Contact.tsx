6ed864cacddea0baa4771f45734b239ff5d27ea0
Cristian Morales <CrisMorales@protonmail.com>
2024-07-22 14:16:45 -0500

    Bigger radius on CTA on hero, didn't like rounded-full so did rounded-3xl.

--- app/ui/contact/Contact.tsx
+++ app/ui/contact/Contact.tsx
@@ -3,7 +3,7 @@ import ContactForm from "./ContactForm";
 export default function Contact() {
 
     return (
-        <section id='contact' className="min-h-contact max-h-fit flex flex-col items-center md:scroll-m-20 w-full">
+        <section id='contact' className="min-h-contact max-h-fit flex flex-col items-center scroll-m-20 w-full">
             <h3 className='text-primary text-3xl mb-4 font-semibold'>
                 Contact Me
             </h3>
